37583245af45f76f8817b654fec405a0f88744f1
fazilajafarli <fezile.ceferli@gmail.com>
2022-06-24 09:28:59 +0400

    final-version

--- js/script.js
+++ js/script.js
@@ -13,26 +13,25 @@ let container2 = document.getElementById('container2');
 let pressKey = document.getElementById('press-key');
 let object = objects[Math.floor(Math.random() * objects.length)];
 let letters = document.getElementById('letters');
-let alphabet = document.getElementById('alphabet')
+let alphabet = document.getElementById('alphabet');
 let answer = [];
-
-maxMistake.innerHTML = 6
-mistakes.innerHTML = 0
+maxMistake.innerHTML = 6;
+mistakes.innerHTML = 0;
 
 for (let i = 0; i < object.length; i++) {
  answer[i] = "_";
 }
-letters.innerHTML = answer.join(' ')
+letters.innerHTML = answer.join(' ');
 
 $(document).ready(function(){
     let buttons = 'abcdefghijklmnopqrstuvwxyz'.split('').map(letter =>
-        `<button class='btn btn-primary px-2 py-2 mx-1 my-1' id="`+letter+`" onclick="guessedLetter(this)">`+letter+`</button>`).join('')
+        `<button class='btn btn-primary px-2 py-2 mx-1 my-1' id="`+letter+`" onclick="guessedLetter(this)">`+letter+`</button>`).join('');
         alphabet.innerHTML = buttons;
 });
 
 function guessedLetter(x){
-    y = object.toLowerCase()
-    pressKey.style.display = 'none'
+    y = object.toLowerCase();
+    pressKey.style.display = 'none';
     if(mistakes.innerHTML < 6){
         if(y.includes(x.innerHTML) == false){
         mistakes.innerHTML = parseInt(mistakes.innerHTML)+1; 
@@ -45,14 +44,14 @@ function guessedLetter(x){
                         x.disabled = true;  
                         if(answer.includes('_') == false){
                             container1.innerHTML = '<p class="blink text-warning fs-1 my-5 py-5">YOU WON!</p>';
-                            container2.innerHTML = '<a href="index.html" class=" btn btn-secondary text-warning fs-4">Restart</a>'
+                            container2.innerHTML = '<a href="index.html" class=" btn btn-secondary text-warning fs-4">Restart</a>';
                         };
                     }; 
                     };
                 };
     }else{
         container1.innerHTML = '<p class="blink text-warning fs-1 my-5 py-5">GAME OVER!</p>';
-        container2.innerHTML = '<a href="index.html" class=" btn btn-secondary text-warning fs-4">Restart</a>'
+        container2.innerHTML = '<a href="index.html" class=" btn btn-secondary text-warning fs-4">Restart</a>';
 
     };
 };
